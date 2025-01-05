6391e7272f883392a442dd22be51414f18b9e069
Gunar <gunarf@yandex.ru>
2025-01-05 12:35:46 +0300

    Lesson 15

--- src/main/java/Lesson_16/MTS2.java
+++ src/main/java/Lesson_16/MTS2.java
@@ -2,20 +2,58 @@ package Lesson_16;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class MTS2 {
     WebDriver driver;
+
     // Конструктор класса
     public MTS2(WebDriver driver) {
         this.driver = driver;
     }
+
     By pay_wrapper = By.xpath("//div[@class='pay__wrapper']/h2");
+
     public String getTitle() {
         return driver.findElement(pay_wrapper).getText().replaceAll("\n", " ");
     }
-    By logoVisa = By.xpath("//img[@alt='Verified by Visa']");
-    public String getVisa() {
-       // return driver.findElement(logoVisa).getText().replaceAll("\n", " ");
-        return driver.findElement(logoVisa);
+
+    By logoVisa = By.xpath("//img[@alt='Visa']");
+    By logoVisa2 = By.xpath("//img[contains(@src, 'visa-verified.svg')]");
+    By logoMasterCard = By.xpath("//img[@alt='MasterCard']");
+    By logoMasterCardSecure = By.xpath("//img[contains(@src, 'mastercard-secure.svg')]");
+    By logoBelkart = By.xpath("//img[@alt='Белкарт']");
+
+    public boolean isLogoPresent(By locator) {
+        return driver.findElements(locator).size() > 0;
+    }
+
+    By serviceLink = By.linkText("Подробнее о сервисе");
+
+    public void clickServiceLink() {
+        driver.findElement(serviceLink).click();
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    By phoneField = By.id("connection-phone");
+    By sumField = By.id("connection-sum");
+    By continueButton = By.cssSelector("button.button.button__default[type='submit']");
+
+    public void fillPhoneField(String phoneNumber) {
+        driver.findElement(phoneField).sendKeys(phoneNumber);
+    }
+
+    public void fillSumField(String sum) {
+        driver.findElement(sumField).sendKeys(sum);
+    }
+
+    public void clickContinueButton() {
+        driver.findElement(continueButton).click();
     }
 }
+
+
+
--- src/test/java/Lesson_16/MTS_Test.java
+++ src/test/java/Lesson_16/MTS_Test.java
@@ -11,11 +11,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MTS_Test {
     MTS2 mts;
-    //MTS2 mts = new MTS2(driver);
     WebDriver driver;
 
     @BeforeEach
@@ -27,25 +26,82 @@ public class MTS_Test {
         driver.manage().window().maximize();
         WebElement cookieLocator = driver.findElement(By.xpath("//*[@id='cookie-agree']"));
         if (cookieLocator.isDisplayed()) {
-        }
             cookieLocator.click();
+        }
     }
+
     @AfterEach
     public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
     }
+
     @DisplayName("Проверка заголовка")
     @Test
     public void FindTitle() {
         String title = "Онлайн пополнение без комиссии";
         assertEquals(title, mts.getTitle(), "Заголовок не найден");
     }
-    @DisplayName("Проверка логотипа")
+
+    @DisplayName("Проверка логотипа Visa")
     @Test
-    public void FindLogo() {
-        String title = "Visa";
-        assertEquals(title, mts.getVisa(), "Лого не найден");
+    public void FindLogoVisa() {
+        assertTrue(mts.isLogoPresent(mts.logoVisa), "Логотип Visa не найден");
+    }
+
+    @DisplayName("Проверка логотипа Verified by Visa")
+    @Test
+    public void FindLogoVisa2() {
+        assertTrue(mts.isLogoPresent(mts.logoVisa2), "Логотип Verified by Visa не найден");
+    }
+
+    @DisplayName("Проверка логотипа MasterCard")
+    @Test
+    public void FindLogoMasterCard() {
+        assertTrue(mts.isLogoPresent(mts.logoMasterCard), "Логотип MasterCard не найден");
+    }
+
+    @DisplayName("Проверка логотипа MasterCardSecure")
+    @Test
+    public void FindLogoMasterCardSecure() {
+        assertTrue(mts.isLogoPresent(mts.logoMasterCardSecure), "Логотип MasterCard не найден");
+    }
+
+    @DisplayName("Проверка логотипа Belkart")
+    @Test
+    public void FindLogoBelkart() {
+        assertTrue(mts.isLogoPresent(mts.logoBelkart), "Логотип Belkart не найден");
+    }
+
+    @DisplayName("Проверка работы ссылки 'Подробнее о сервисе'")
+    @Test
+    public void CheckServiceLink() {
+        mts.clickServiceLink();
+        String expectedUrl = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
+        assertEquals(expectedUrl, mts.getCurrentUrl(), "URL не совпадает");
+    }
+
+    @DisplayName("Проверка работы кнопки 'Продолжить'")
+    @Test
+    public void CheckContinueButton() {
+        String phoneNumber = "297777777";
+        String sum = "10";
+
+        mts.fillPhoneField(phoneNumber);
+        mts.fillSumField(sum);
+        mts.clickContinueButton();
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+
+
+
