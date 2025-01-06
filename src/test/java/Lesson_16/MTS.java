bb850a398462324ee27a8f52836d6a6044f29648
Gunar <gunarf@yandex.ru>
2025-01-06 14:32:48 +0300

    Lesson 18

--- src/test/java/Lesson_16/MTS.java
+++ src/test/java/Lesson_16/MTS.java
@@ -1,6 +1,7 @@
 package Lesson_16;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -53,6 +54,9 @@ public class MTS {
 
     @DisplayName("Проверка заголовка")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Проверка заголовка на странице оплаты")
     public void FindTitle() {
         String title = "Онлайн пополнение без комиссии";
         assertEquals(title, paymentPage.getTitle(), "Заголовок не найден");
@@ -60,36 +64,54 @@ public class MTS {
 
     @DisplayName("Проверка логотипа Visa")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка наличия логотипа Visa")
     public void FindLogoVisa() {
         assertTrue(paymentPage.isLogoPresent(paymentPage.logoVisa), "Логотип Visa не найден");
     }
 
     @DisplayName("Проверка логотипа Verified by Visa")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка наличия логотипа Verified by Visa")
     public void FindLogoVisa2() {
         assertTrue(paymentPage.isLogoPresent(paymentPage.logoVisa2), "Логотип Verified by Visa не найден");
     }
 
     @DisplayName("Проверка логотипа MasterCard")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка наличия логотипа MasterCard")
     public void FindLogoMasterCard() {
         assertTrue(paymentPage.isLogoPresent(paymentPage.logoMasterCard), "Логотип MasterCard не найден");
     }
 
     @DisplayName("Проверка логотипа MasterCardSecure")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка наличия логотипа MasterCardSecure")
     public void FindLogoMasterCardSecure() {
         assertTrue(paymentPage.isLogoPresent(paymentPage.logoMasterCardSecure), "Логотип MasterCard не найден");
     }
 
     @DisplayName("Проверка логотипа Belkart")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка наличия логотипа Belkart")
     public void FindLogoBelkart() {
         assertTrue(paymentPage.isLogoPresent(paymentPage.logoBelkart), "Логотип Belkart не найден");
     }
 
     @DisplayName("Проверка работы ссылки 'Подробнее о сервисе'")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка работы ссылки 'Подробнее о сервисе'")
     public void CheckServiceLink() {
         paymentPage.clickServiceLink();
         String expectedUrl = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
@@ -98,6 +120,9 @@ public class MTS {
 
     @DisplayName("Проверка работы кнопки 'Продолжить'")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка работы кнопки 'Продолжить'")
     public void CheckContinueButton() {
         String phoneNumber = "297777777";
         String sum = "10";
@@ -115,6 +140,9 @@ public class MTS {
 
     @DisplayName("Проверка надписей в незаполненных полях")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка надписей в незаполненных полях")
     public void CheckEmptyFieldPlaceholders() {
         assertEquals("Номер телефона", paymentPage.getPhoneFieldPlaceholder(), "Неверная надпись в поле 'Номер телефона'");
         assertEquals("Сумма", paymentPage.getSumFieldPlaceholder(), "Неверная надпись в поле 'Сумма'");
@@ -123,6 +151,9 @@ public class MTS {
 
     @DisplayName("Проверка заполнения полей и отображения ошибок")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка заполнения полей и отображения ошибок")
     public void CheckFieldValidation() {
         //paymentPage.selectService("Услуги связи");
         paymentPage.fillPhoneField("invalid");
@@ -137,6 +168,9 @@ public class MTS {
 
     @DisplayName("Проверка корректности отображения суммы и номера телефона")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка корректности отображения суммы и номера телефона")
     public void CheckPaymentDetails() {
         //paymentPage.selectService("Услуги связи");
         paymentPage.fillPhoneField("297777777");
@@ -152,12 +186,14 @@ public class MTS {
 
         paymentIframe.switchToIframe();
 
-        // Проверка корректности отображения суммы и номера телефона
-        // Добавьте необходимые проверки здесь
+
     }
 
     @DisplayName("Проверка ошибок в полях карты")
     @Test
+    @Owner("Gunar")
+    @Severity(SeverityLevel.NORMAL)
+    @Step("Проверка ошибок в полях карты")
     public void CheckCardFieldErrors() {
         //paymentPage.selectService("Услуги связи");
         paymentPage.fillPhoneField("297777777");
@@ -184,3 +220,4 @@ public class MTS {
         assertEquals("Введите имя и фамилию как указано на карте", paymentIframe.getHolderNameError(), "Неверная ошибка в поле 'Имя держателя'");
     }
 }
+
